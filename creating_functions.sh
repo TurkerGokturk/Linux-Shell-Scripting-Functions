1050fb6ed8b63198f75689b1d490169808b29dd0
Trkr_Gktrk <turkergokturk@gmail.com>
2023-01-31 23:39:48 +0100

    creating_functions.sh added

---
+++ creating_functions.sh
@@ -0,0 +1,14 @@
+#A Bash function is a piece of code that only runs when it is called. 
+#Functions enable you to reuse code. We can call the functions numerous times.
+
+# Create a folder and name it functions.
+
+# It is pretty easy to declare and call a function.
+
+#!/bin/bash
+
+Welcome () {
+    echo "Welcome to Linux Lessons"
+}
+
+Welcome
